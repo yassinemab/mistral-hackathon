0273e3b8a27347115a110ab4f6de028a2d0f8c98
Yassine Mabrouk <yassinemab04@gmail.com>
2024-04-08 11:00:03 +0200

    added pdf overview page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import UploadPDF from "./pages/UploadPDF";
+import PDFOverview from "./pages/PDFOverview";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="" element={<Home />} />
+        <Route path="/documents/upload" element={<UploadPDF />} />
+        <Route path="/documents" element={<PDFOverview />} />
       </Routes>
     </BrowserRouter>
   );
