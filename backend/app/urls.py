560b93b6501360b49b95f4560454b09604a71b51
Gijsbert Westeneng <gijsbert@bitonic.nl>
2024-04-08 15:57:48 +0200

    data

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views.PostDataView import PostDataDB
 
 urlpatterns = [
     # path('post/', PostViewSet.as_view({'get': 'getPosts', "post": "insertPost", "delete": "deletePost"})),
+    path('get_data/', PostDataDB.as_view({"get": "getRecords"})),
+    path('post_data/', PostDataDB.as_view({"post": "PostData"})),
+    path('post_pdf/', PostDataDB.as_view({"post": "PostPDF"})),
 ]
