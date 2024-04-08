5515a61c8ea75b49c7520128c45b074e82a8e0b8
tassalor1 <walkerconnor14@gmail.com>
2024-04-08 10:39:58 +0200

    load and models

--- backend/functions/load_pdf.py
+++ backend/functions/load_pdf.py
@@ -5,13 +5,11 @@ import base64
 import tempfile
 
 
-
 def read_pdf(document):
     buffer = base64.b64decode(document.content)
     with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as tmpfile:
         tmpfile.write(buffer)
         temp_pdf_path = tmpfile.name
-
     pages = PyPDFLoader.load_and_split() 
     page_text = [document.page_content for document in pages] if hasattr(
         pages[0], 'page_content') else ""
